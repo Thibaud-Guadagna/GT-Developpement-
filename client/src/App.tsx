577ef459220790be55f43c30b575b131e8ddb89a
thibaud.guadagna <thibaud.guadagna@gmail.com>
2025-07-03 13:14:54 +0200

    Fix Vercel deploiement suite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import About from '../components/About';
 import Contact from '../components/Contact';
 import Header from '../components/Header';
 import Projets from '../components/Projects';
+
 import Footer from '../components/Footer'
 
 import './main';
